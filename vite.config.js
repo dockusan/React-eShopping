4a789c12cc6fd5fe16a64a913c32bfad6a646ed5
Duc Nguyen <ducanguyen@kms-healthcare.com>
2024-12-01 16:02:23 +0700

    Update base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "https://dockusan.github.io/React-eShopping/",
+  base: "React-eShopping",
   plugins: [react()],
 });
